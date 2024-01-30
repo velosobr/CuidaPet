f4c2ca698861b65075b643cc6c36c525d4d6a5ae
velosobr <linoc.veloso@gmail.com>
2024-01-30 20:14:16 -0300

    Criando e configurando projeto e suas dependências

---
+++ cuidapet_mobile/lib/app/app_module.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'home_page.dart';
+
+class AppModule extends Module{
+
+@override
+  void binds(i) {}
+
+ @override
+  void routes(r) {
+    r.child('/', child: (context) => const HomePage());
+  }
+  
+}
